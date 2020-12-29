1ca4cfa48ba5360beb07d735c88e93c4d296b6eb
Aseelsamer <assoul_08@hotmail.com>
2020-12-29 14:36:13 +0200

    lab02

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const Notes = require('./lib/notes.js');
 const yourAdds = new Input();
 const yourNotes = new Notes(yourAdds);
 
-// yourAdds.validateMethod();
-yourAdds.addNote();
+// // yourAdds.validateMethod();
+// yourAdds.addNote();
 
 // yourNotes.execute();
 // yourNotes.add();
