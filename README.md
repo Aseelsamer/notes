ec821d2fa1edb63db8167bd7e4ec66a764466520
Aseelsamer <assoul_08@hotmail.com>
2020-12-31 00:56:36 +0200

    readme

--- README.md
+++ README.md
@@ -16,6 +16,12 @@ minimist
 For example : node index.js --add 'some note'
 
 or you can say :node index.js -a 'some note'
+to list every note in the Database :`node index.js --list`
+ to list specifice categoery : `node index.js --list categories `
+
+ to delete a note : `node index.js --delete 'id' `
+
+ to update note :`node index.js --update 'updated note' --id ' id'` 
 
 
 #### How to use your library :
--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,21 +1,51 @@
 'use strict';
-const Notes = require('../lib/notes');
+const Collection = require('../modules/notes-collection');
+const collection = new Collection();
 
 jest.spyOn(global.console, 'log');
 
-describe('Notes Module', () => {
-    it('if the note gives console', () => {
-        let obj = { action: 'add', payload: 'note' };
-        let note = new Notes(obj);
-        note.execute(obj);
-        expect(console.log).toHaveBeenCalled();
+require('@code-fellows/supergoose');
+
+describe('note collection', () => {
+    let object = {text:'test1',categories: school}
+  it('can create() a new note', () => {
+    return collection.create(obj)
+      .then(result => {
+          console.log("result : ", result)
+        Object.keys(obj).forEach(key => {
+          expect(result[key]).toEqual(object[key]);
+        });
+      });
+  });
+
+it('can get() list of notes',()=>{
+    return collection.create(object)
+    .then(result=>{
+        return collection.get(result.categories)
+        .then(list=>{
+            Object.keys(object).forEach(key=>{
+                expect(list[key].toEqual(obj[key]))
+            })
+        });
     })
+})
+
+
 });
 
-it('if the note gives console', () => {
-    let obj = {note: 'error:You have something wrong'}
-    let note = new Notes(obj);
-    note.execute(obj);
-    expect(console.log).toHaveBeenCalled();
-});
+// describe('Notes Module', () => {
+//     it('if the note gives console', () => {
+//         let obj = { action: 'add', payload: 'note' };
+//         let note = new Notes(obj);
+//         note.execute(obj);
+//         expect(console.log).toHaveBeenCalled();
+//     })
+// });
+
+// it('if the note gives console', () => {
+//     let obj = {note: 'error:You have something wrong'}
+//     let note = new Notes(obj);
+//     note.execute(obj);
+//     expect(console.log).toHaveBeenCalled();
+// });
 
