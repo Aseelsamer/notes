5b7d6032ba065dd0943a39e9e60c2ad2130cea58
Aseelsamer <assoul_08@hotmail.com>
2020-12-30 07:27:38 +0200

    lab3

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 'use strict';
+const mongoose = require('mongoose');
+const Note = require('./modules/note-schema.js');
 
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
 
+
+
+const MONGODB_URI = 'mongodb://localhost:27017/mynote';
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
+
+
 const yourAdds = new Input();
 const yourNotes = new Notes(yourAdds);
+    
+    
+    mongoose.disconnect()
 
-// // yourAdds.validateMethod();
-// yourAdds.addNote();
 
-// yourNotes.execute();
-// yourNotes.add();
+
